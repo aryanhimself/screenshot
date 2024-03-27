04546e49a8cabc1fcba8d16dc995ac6e1cf41135
Aryan Phuyal <118953390+aryanhimself@users.noreply.github.com>
2024-03-27 13:20:35 +0545

    fix: issue

--- service/generateImage.js
+++ service/generateImage.js
@@ -7,7 +7,6 @@ exports.generateImage = (url, width, height) => {
     try {
       const browser = await puppeteer.launch({
         headless: true,
-        args: ["--no-sandbox", "--disable-setuid-sandbox"],
       });
 
       const page = await browser.newPage();
