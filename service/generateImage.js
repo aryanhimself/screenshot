d777de1f27d5c377ab8e6d5074f3c1d894a247e7
Aryan Phuyal <118953390+aryanhimself@users.noreply.github.com>
2024-04-02 13:02:38 +0545

    fix nosandbox in puppiter

--- service/generateImage.js
+++ service/generateImage.js
@@ -7,6 +7,7 @@ exports.generateImage = (url, width, height) => {
     try {
       const browser = await puppeteer.launch({
         headless: true,
+        args: ["--no-sandbox"],
       });
 
       const page = await browser.newPage();
