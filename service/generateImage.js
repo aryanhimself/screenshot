42fcece196609830ecc11c96d2487f0b1ea91a92
Aryan Phuyal <118953390+aryanhimself@users.noreply.github.com>
2024-05-13 16:34:47 +0545

    fix: og generation issue on local fix

--- service/generateImage.js
+++ service/generateImage.js
@@ -2,36 +2,34 @@ const puppeteer = require("puppeteer");
 const ejs = require("ejs");
 const path = require("path");
 
-exports.generateImage = (url, width, height) => {
-  return new Promise(async (resolve, reject) => {
-    try {
-      const browser = await puppeteer.launch({
-        headless: true,
-        args: ["--no-sandbox", "--enable-gpu"],
-        ignoreDefaultArgs: ["--disable-extensions"],
-      });
+exports.generateImage = async (url, width, height) => {
+  try {
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: ["--no-sandbox", "--enable-gpu"],
+      ignoreDefaultArgs: ["--disable-extensions"],
+    });
 
-      const page = await browser.newPage();
-      await page.goto(url, {
-        waitUntil: "networkidle0",
-      });
+    const page = await browser.newPage();
+    await page.goto(url, {
+      waitUntil: "networkidle0",
+    });
 
-      const divElement = await page.waitForSelector("div");
-      const divBoundingBox = await divElement.boundingBox();
+    const divElement = await page.waitForSelector("div");
+    const divBoundingBox = await divElement.boundingBox();
 
-      const image = await page.screenshot({
-        type: "jpeg",
-        clip: {
-          x: divBoundingBox.x,
-          y: divBoundingBox.y,
-          width: divBoundingBox.width,
-          height: divBoundingBox.height,
-        },
-      });
-      await browser.close();
-      resolve(image);
-    } catch (err) {
-      reject(err);
-    }
-  });
+    const image = await page.screenshot({
+      type: "jpeg",
+      clip: {
+        x: divBoundingBox.x,
+        y: divBoundingBox.y,
+        width: divBoundingBox.width,
+        height: divBoundingBox.height,
+      },
+    });
+    await browser.close();
+    return image;
+  } catch (err) {
+    console.log(err);
+  }
 };
